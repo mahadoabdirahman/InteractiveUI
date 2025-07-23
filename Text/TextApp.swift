9e57dee23ed2bcfe2b0551d8a2f9d5e22eaa3888
Mahado <mabdirahman242424@gmail.com>
2025-07-23 13:39:50 -0700

    Initial Commit

---
+++ Text/TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextApp.swift
+//  Text
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TextTests/TextTests.swift
@@ -0,0 +1,17 @@
+//
+//  TextTests.swift
+//  TextTests
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import Testing
+@testable import Text
+
+struct TextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
